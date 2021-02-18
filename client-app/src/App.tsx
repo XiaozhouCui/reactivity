247f4d86db7083c33b464e359f6c5e35c03d0e58
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-18 20:34:27 +1000

    Integrated semantic-ui-react

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import axios from 'axios'
+import { Header, List } from 'semantic-ui-react'
 
 function App() {
   const [activities, setActivities] = useState([])
@@ -14,17 +14,13 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>
-              {activity.title}
-            </li>
-          ))}
-        </ul>
-      </header>
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
     </div>
   )
 }
