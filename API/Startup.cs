5cba1cae5dfc5722b9a4f6163c859d8d741611fa
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-18 20:18:42 +1000

    Added CORS and fetched data from API

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,14 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 
             });
+            // add CORS header
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +62,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
