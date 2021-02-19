83f559f535852ded07c4a902766dcc856af29286
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-19 15:08:46 +1000

    Refactor: thin controllers in the API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -3,29 +3,19 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        // Initialise field from parameter
-        private readonly IMediator _mediator;
-        // Inject Mediator into constructor
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         // get a list of activities
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             // get response from mediator, initiate an instance of List in Activities
-            return await _mediator.Send(new List.Query());
+            // Mediator is the protected property in parent class BaseApiController
+            return await Mediator.Send(new List.Query());
             // return await _context.Activities.ToListAsync();
         }
 
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Controllers
 {
@@ -6,6 +8,11 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        // add fields/properties to be used in derived classes
+        private IMediator _mediator;
+        // protected property Mediator: dependency injection for MediatR?
+        // ??= means if _mediator is null, use the right hand side expression
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
     }
 }
