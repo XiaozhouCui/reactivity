6f783f94388928232c75e361036cd4987c2495de
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-21 14:46:31 +1000

    Added ts interface Activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react'
+import { Activity } from './models/activity'
 
 function App() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then((response) => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
       setActivities(response.data)
     })
   }, [])
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Header as="h2" icon="users" content="Reactivities" />
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
