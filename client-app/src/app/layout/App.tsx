0352f0efbf2f10d24fdab09d3c9efa962ab24b3b
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-21 15:36:18 +1000

    Added NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react'
-import { Activity } from './models/activity'
+import { Activity } from '../models/activity'
+import NavBar from './NavBar'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data)
-    })
+    axios
+      .get<Activity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        setActivities(response.data)
+      })
   }, [])
 
   return (
     <div>
-      <Header as="h2" icon="users" content="Reactivities" />
+      <NavBar />
       <List>
         {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button, Container, Menu } from 'semantic-ui-react'
+
+const NavBar = () => {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item header>
+          <img src="assets/logo.png" alt="logo" />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button positive content="Create Activity" />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
+
+export default NavBar
