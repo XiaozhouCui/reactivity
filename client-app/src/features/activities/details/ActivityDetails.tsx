de0df5980d453b5df8582fc03e32bb97d947533c
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-22 13:55:41 +1000

    Adding ActivityDetails view

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button, Card, Image } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+
+interface Props {
+  activity: Activity
+}
+
+const ActivityDetails = ({ activity }: Props) => {
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <Card.Content>
+        <Card.Header>{activity.title}</Card.Header>
+        <Card.Meta>
+          <span>{activity.date}</span>
+        </Card.Meta>
+        <Card.Description>{activity.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group width="2">
+          <Button basic color="blue" content='Edit' />
+          <Button basic color="grey" content='Cancel' />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  )
+}
+
+export default ActivityDetails
