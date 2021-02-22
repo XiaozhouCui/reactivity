69de307e460c0534ab5bfb309d61fc45b6bebb07
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-22 11:47:01 +1000

    Added ActivitiesList

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Container, List } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { Activity } from '../models/activity'
 import NavBar from './NavBar'
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -19,11 +20,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities} />
       </Container>
     </>
   )
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+import ActivityList from './ActivityList'
+
+interface Props {
+  activities: Activity[]
+}
+
+const ActivityDashboard = ({ activities }: Props) => {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} />
+      </Grid.Column>
+    </Grid>
+  )
+}
+
+export default ActivityDashboard
