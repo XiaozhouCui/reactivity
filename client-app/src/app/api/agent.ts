4cd7681d31b1e05e87ab4d7c18ed69fe8aca65bc
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-22 17:43:03 +1000

    Setting up axios agent

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:5000/api'
+
+const responseBody = (response: AxiosResponse) => response.data
+
+const requests = {
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url).then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
+}
+
+const Activities = {
+  list: () => requests.get('/activities'),
+}
+
+const agent = { Activities }
+
+export default agent
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
 import { Container } from 'semantic-ui-react'
 import { Activity } from '../models/activity'
 import NavBar from './NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
 import { v4 as uuid } from 'uuid'
+import agent from '../api/agent'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
@@ -14,11 +14,9 @@ function App() {
   const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
-    axios
-      .get<Activity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
-        setActivities(response.data)
-      })
+    agent.Activities.list().then((response) => {
+      setActivities(response)
+    })
   }, [])
 
   const handleSelectActivity = (id: string) => {
