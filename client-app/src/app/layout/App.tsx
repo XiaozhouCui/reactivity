ac4e82e6845df58b90239b68f8ee7f3dfe000a07
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-24 09:54:25 +1000

    Added MobX action to update state

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container } from 'semantic-ui-react'
+import { Button, Container } from 'semantic-ui-react'
 import { Activity } from '../models/activity'
 import NavBar from './NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
@@ -7,6 +7,7 @@ import { v4 as uuid } from 'uuid'
 import agent from '../api/agent'
 import LoadingComponent from './LoadingComponent'
 import { useStore } from '../stores/store'
+import { observer } from 'mobx-react-lite'
 
 function App() {
   const { activityStore } = useStore()
@@ -87,6 +88,7 @@ function App() {
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <h2>{activityStore.title}</h2>
+        <Button content='Add exclamation!' positive onClick={activityStore.setTitle} />
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -104,4 +106,5 @@ function App() {
   )
 }
 
-export default App
+// use mobx to make App an Observer, to listen to observables in mobx
+export default observer(App)
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,21 @@
-import { makeObservable, observable } from "mobx"
+import { makeAutoObservable } from 'mobx'
 
 export default class ActivityStore {
-    title = 'Hello from MobX!'
-    constructor() {
-        // makeObservable will use "this" class
-        makeObservable(this, {
-            title: observable
-        })
-    }
+  // MobX Observable: class properties
+  title = 'Hello from MobX!'
+  
+  constructor() {
+    // makeAutoObservable will use "this" class
+    makeAutoObservable(this)
+    // makeObservable(this, {
+    //   title: observable,
+    //   setTitle: action
+    // })
+  }
+
+  // MobX Action: class methods
+  // arrow function auto-bind setTitle() to "this" class
+  setTitle = () => {
+    this.title = this.title + '!'
+  }
 }
