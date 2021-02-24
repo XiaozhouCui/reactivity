7b590c0bf86f926d46ec6e1c5cf385721eb36df1
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-24 15:09:13 +1000

    Added Routes and NavLinks

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,20 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Container } from 'semantic-ui-react'
 import NavBar from './NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
-import LoadingComponent from './LoadingComponent'
-import { useStore } from '../stores/store'
 import { observer } from 'mobx-react-lite'
+import { Route } from 'react-router-dom'
+import HomePage from '../../features/home/HomePage'
+import ActivityForm from '../../features/activities/form/ActivityForm'
 
 function App() {
-  const { activityStore } = useStore()
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore])
-
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/activities' component={ActivityDashboard} />
+        <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </>
   )
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Grid } from 'semantic-ui-react'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { useStore } from '../../../app/stores/store'
 import ActivityDetails from '../details/ActivityDetails'
 import ActivityForm from '../form/ActivityForm'
@@ -9,6 +10,13 @@ import ActivityList from './ActivityList'
 const ActivityDashboard = () => {
   const { activityStore } = useStore()
   const { selectedActivity, editMode } = activityStore
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore])
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
 
   return (
     <Grid>
