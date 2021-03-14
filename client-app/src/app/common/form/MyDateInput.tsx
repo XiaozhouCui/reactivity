2fceafc7a1e7f8a32333017dabaea994cf8600ba
Joe Cui <xiaozhou.cui@gmail.com>
2021-03-14 16:44:53 +1000

    Added re-usable date input with react-datepicker

---
+++ client-app/src/app/common/form/MyDateInput.tsx
@@ -0,0 +1,26 @@
+import { useField } from 'formik'
+import { Form, Label } from 'semantic-ui-react'
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker'
+
+// Re-usable date picker
+// Partial<ReactDatePickerProps> will make all the props optional
+const MyDateInput = (props: Partial<ReactDatePickerProps>) => {
+  const [field, meta, helpers] = useField(props.name!)
+  return (
+    <Form.Field error={meta.touched && !!meta.error}>
+      <DatePicker
+        {...field}
+        {...props}
+        selected={(field.value && new Date(field.value)) || null}
+        onChange={(value) => helpers.setValue(value)}
+      />
+      {meta.touched && meta.error ? (
+        <Label basic color='red'>
+          {meta.error}
+        </Label>
+      ) : null}
+    </Form.Field>
+  )
+}
+
+export default MyDateInput
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -11,6 +11,7 @@ import MyTextInput from '../../../app/common/form/MyTextInput'
 import MyTextArea from '../../../app/common/form/MyTextArea'
 import MySelectInput from '../../../app/common/form/MySelectInput'
 import { categoryOptions } from '../../../app/common/options/categoryOptions'
+import MyDateInput from '../../../app/common/form/MyDateInput'
 
 const ActivityForm = () => {
   const { activityStore } = useStore()
@@ -79,8 +80,18 @@ const ActivityForm = () => {
           <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
             <MyTextInput name='title' placeholder='Title' />
             <MyTextArea rows={3} placeholder='Description' name='description' />
-            <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
-            <MyTextInput placeholder='Date' name='date' />
+            <MySelectInput
+              options={categoryOptions}
+              placeholder='Category'
+              name='category'
+            />
+            <MyDateInput
+              placeholderText='Date'
+              name='date'
+              showTimeSelect
+              timeCaption='time'
+              dateFormat='MMMM d, yyyy h:mm aa'
+            />
             <MyTextInput placeholder='City' name='city' />
             <MyTextInput placeholder='Venue' name='venue' />
             <Button
