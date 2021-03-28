4fa478e663a042e7c5df0c0f6684ba550790ce27
Joe Cui <xiaozhou.cui@gmail.com>
2021-03-28 16:13:09 +1000

    Created user interfaces and methods

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,8 +1,9 @@
-import { Activity } from '../models/activity'
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import { toast } from 'react-toastify'
 import { history } from '../..'
 import { store } from '../stores/store'
+import { Activity } from '../models/activity'
+import { User, UserFormValues } from '../models/user'
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -78,6 +79,13 @@ const Activities = {
   delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 }
 
-const agent = { Activities }
+const Account = {
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+  register: (user: UserFormValues) =>
+    requests.post<User>('/account/register', user),
+}
+
+const agent = { Activities, Account }
 
 export default agent
