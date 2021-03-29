890dfe6c09cd4141872f04ac19d44ccbc3b3b54c
Joe Cui <xiaozhou.cui@gmail.com>
2021-03-29 20:04:43 +1000

    Display errors in the form

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,7 +1,7 @@
-import { Form, Formik } from 'formik'
+import { ErrorMessage, Form, Formik } from 'formik'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Label } from 'semantic-ui-react'
 import MyTextInput from '../../app/common/form/MyTextInput'
 import { useStore } from '../../app/stores/store'
 
@@ -9,14 +9,35 @@ const LoginForm = () => {
   const { userStore } = useStore()
   return (
     <Formik
-      initialValues={{ email: '', password: '' }}
-      onSubmit={(values) => userStore.login(values)}
+      initialValues={{ email: '', password: '', error: null }}
+      onSubmit={(values, { setErrors }) =>
+        userStore
+          .login(values)
+          .catch((error) => setErrors({ error: 'Invalid email or password' }))
+      }
     >
-      {({ handleSubmit, isSubmitting }) => (
+      {({ handleSubmit, isSubmitting, errors }) => (
         <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
           <MyTextInput name='email' placeholder='Email' />
           <MyTextInput name='password' placeholder='Password' type='password' />
-          <Button loading={isSubmitting} positive content='Login' type='submit' fluid />
+          <ErrorMessage
+            name='error'
+            render={() => (
+              <Label
+                style={{ marginBottom: 10 }}
+                basic
+                color='red'
+                content={errors.error}
+              />
+            )}
+          />
+          <Button
+            loading={isSubmitting}
+            positive
+            content='Login'
+            type='submit'
+            fluid
+          />
         </Form>
       )}
     </Formik>
