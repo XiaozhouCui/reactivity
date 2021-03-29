f01283acffee0f243283f3672ac2077b59188651
Joe Cui <xiaozhou.cui@gmail.com>
2021-03-30 09:24:23 +1000

    Updating the HomePage and NavBar

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,20 +1,51 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { Button, Container, Menu } from 'semantic-ui-react'
+import { Link, NavLink } from 'react-router-dom'
+import { Button, Container, Dropdown, Image, Menu } from 'semantic-ui-react'
+import { useStore } from '../stores/store'
 
 const NavBar = () => {
+  const {
+    userStore: { user, logout },
+  } = useStore()
   return (
     <Menu inverted fixed='top'>
       <Container>
         <Menu.Item as={NavLink} exact to='/' header>
-          <img src='/assets/logo.png' alt='logo' style={{ marginRight: '10px' }} />
+          <img
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginRight: '10px' }}
+          />
           Reactivities
         </Menu.Item>
         <Menu.Item as={NavLink} to='/activities' name='Activities' />
         <Menu.Item as={NavLink} to='/errors' name='Errors' />
         <Menu.Item>
-          <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+          <Button
+            as={NavLink}
+            to='/createActivity'
+            positive
+            content='Create Activity'
+          />
+        </Menu.Item>
+        <Menu.Item position='right'>
+          <Image
+            src={user?.image || '/assets/user.png'}
+            avatar
+            spaced='right'
+          />
+          <Dropdown pointing='top left' text={user?.displayName}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to={`/profile/${user?.username}`}
+                text='My Profile'
+                icon='user'
+              />
+              <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+            </Dropdown.Menu>
+          </Dropdown>
         </Menu.Item>
       </Container>
     </Menu>
--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,22 +1,38 @@
+import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Container, Header, Image, Segment } from 'semantic-ui-react'
+import { useStore } from '../../app/stores/store'
 
 const HomePage = () => {
+  const { userStore } = useStore()
   return (
     <Segment inverted textAlign='center' vertical className='masthead'>
       <Container text>
         <Header as='h1' inverted>
-          <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
+          <Image
+            size='massive'
+            src='/assets/logo.png'
+            alt='logo'
+            style={{ marginBottom: 12 }}
+          />
           Reactivities
         </Header>
-        <Header as='h2' inverted content='Welcom to Reactivities' />
-        <Button as={Link} to='/login' size='huge' inverted>
-          Login!
-        </Button>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as='h2' inverted content='Welcom to Reactivities' />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Go to Actvities!
+            </Button>
+          </>
+        ) : (
+          <Button as={Link} to='/login' size='huge' inverted>
+            Login!
+          </Button>
+        )}
       </Container>
     </Segment>
   )
 }
 
-export default HomePage
+export default observer(HomePage)
