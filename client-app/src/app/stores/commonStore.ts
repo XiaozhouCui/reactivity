f1dfa3185890c755ce1b1811a190c1707b8196f2
Joe Cui <xiaozhou.cui@gmail.com>
2021-03-29 20:17:23 +1000

    Setting the token upon login

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -4,6 +4,9 @@ import { ServerError } from './../models/serverError'
 export default class CommonStore {
   // error is made an observable
   error: ServerError | null = null
+  // store the token in common store
+  token: string | null = null
+  appLoaded = false
 
   constructor() {
     makeAutoObservable(this)
@@ -11,5 +14,14 @@ export default class CommonStore {
 
   setServerError = (error: ServerError) => {
     this.error = error
+  }
+
+  setToken = (token: string | null) => {
+    if (token) window.localStorage.setItem('jwt', token)
+    this.token = token
+  }
+
+  setAppLoaded = () => {
+    this.appLoaded = true
   }
 }
--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from 'mobx'
+import { makeAutoObservable, runInAction } from 'mobx'
+import { history } from '../..'
 import agent from '../api/agent'
 import { User, UserFormValues } from '../models/user'
+import { store } from './store'
 
 export default class UserStore {
   user: User | null = null
@@ -17,9 +19,20 @@ export default class UserStore {
   login = async (creds: UserFormValues) => {
     try {
       const user = await agent.Account.login(creds)
-      console.log(user)
+      // save the token in common store
+      store.commonStore.setToken(user.token)
+      // modify an observable inside an action
+      runInAction(() => this.user = user)
+      history.push('/activities')
     } catch (error) {
       throw error
     }
+  }
+
+  logout = () => {
+    store.commonStore.setToken(null)
+    window.localStorage.removeItem('jwt')
+    this.user = null
+    history.push('/')
   }
 }
