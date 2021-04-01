d4c66d3f1ffec5b90840093144fefbe2f2d3edfb
Joe Cui <xiaozhou.cui@gmail.com>
2021-04-01 12:29:08 +1000

    Adding the attendees component

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Icon, Item, Segment } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
+import ActivityListItemAttendee from './ActivityListItemAttendee'
 
 interface Props {
   activity: Activity
@@ -30,7 +31,9 @@ const ActivityListItem = ({ activity }: Props) => {
           <Icon name='marker' /> {activity.venue}
         </span>
       </Segment>
-      <Segment secondary>Attendees go here</Segment>
+      <Segment secondary>
+        <ActivityListItemAttendee attendees={activity.attendees!} />
+      </Segment>
       <Segment clearing>
         <span>{activity.description}</span>
         <Button
