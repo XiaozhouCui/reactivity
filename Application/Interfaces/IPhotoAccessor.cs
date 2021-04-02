4ac9d68d3a058d2eb3894ced5fe349b59ef7cbca
Joe Cui <xiaozhou.cui@gmail.com>
2021-04-02 11:24:46 +1000

    Adding the add photo logic using Cloudinary SDK

--- Application/Interfaces/IPhotoAccessor.cs
+++ Application/Interfaces/IPhotoAccessor.cs
@@ -10,6 +10,7 @@ namespace Application.Interfaces
         // IFormFile represents the file sent with the HTTP request
         // it comes with file properties like size, name etc.
          Task<PhotoUploadResult> AddPhoto(IFormFile file);
+         // delete a photo from Cloudinary
          Task<string> DeletePhoto(string publicId);
     }
 }
--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,20 +1,73 @@
+using System;
 using System.Threading.Tasks;
 using Application.Interfaces;
 using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Photos
 {
+    // constructor: give access to the Cloudinary configuration
     public class PhotoAccessor : IPhotoAccessor
     {
-        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        // Cloudinary SDK: CloudinaryDotNet
+        private readonly Cloudinary _cloudinary;
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
         {
-            throw new System.NotImplementedException();
+            // create a new Cloudinary account, order inside Account() is important
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+            // create a new Cloudinary object
+            _cloudinary = new Cloudinary(account);
         }
 
-        public Task<string> DeletePhoto(string publicId)
+        // implement methods from IPhotoAccessor
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile file)
         {
-            throw new System.NotImplementedException();
+            // make sure there is a file to work with
+            if (file.Length > 0)
+            {
+                // "using" means the stream will be disposed of when finished
+                await using var stream = file.OpenReadStream();
+                // prepare upload parameters
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    // let Cloudinary to transform the file into a square image
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+                // upload the file
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+                // if upload failed, throw error
+                if (uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+
+                // return successful result
+                return new PhotoUploadResult
+                {
+                    PublicId = uploadResult.PublicId,
+                    // get HTTPS version of url
+                    Url = uploadResult.SecureUrl.ToString()
+                };
+            }
+
+            // if there is no file to upload, return null
+            return null;
+        }
+
+        public async Task<string> DeletePhoto(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
