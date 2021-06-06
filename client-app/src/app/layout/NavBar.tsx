e4a79eef718e36505570c877634df61354a2330e
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-06 17:24:39 +1000

    Wiring up profile page

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -39,7 +39,7 @@ const NavBar = () => {
             <Dropdown.Menu>
               <Dropdown.Item
                 as={Link}
-                to={`/profile/${user?.username}`}
+                to={`/profiles/${user?.username}`}
                 text='My Profile'
                 icon='user'
               />
---
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePage = () => {
+    return (
+        <h1>
+            Profile
+        </h1>
+    )
+}
+
+export default ProfilePage
