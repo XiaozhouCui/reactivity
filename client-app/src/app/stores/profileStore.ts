d5cafcc89a03b038afbe687cf17ddbffda7aa9c3
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-10 22:53:46 +1000

    Setting the main photo function added

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,12 +1,13 @@
 import { makeAutoObservable, runInAction } from 'mobx'
 import agent from '../api/agent'
-import { Profile } from '../models/profile'
+import { Photo, Profile } from '../models/profile'
 import { store } from './store'
 
 export default class ProfileStore {
   profile: Profile | null = null
   loadingProfile = false
   uploading = false
+  loading = false
 
   constructor() {
     makeAutoObservable(this)
@@ -55,6 +56,25 @@ export default class ProfileStore {
     } catch (error) {
       console.log(error)
       runInAction(() => (this.uploading = false))
+    }
+  }
+
+  setMainPhoto = async (photo: Photo) => {
+    this.loading = true
+    try {
+      await agent.Profiles.setMainPhoto(photo.id)
+      store.userStore.setImage(photo.url)
+      runInAction(() => {
+        if (this.profile && this.profile.photos) {
+          this.profile.photos.find(p => p.isMain)!.isMain = false
+          this.profile.photos.find(p => p.id === photo.id)!.isMain = true
+          this.profile.image = photo.url
+          this.loading = false
+        }
+      })
+    } catch (error) {
+      console.log(error)
+      runInAction(() => (this.loading = false))
     }
   }
 }
