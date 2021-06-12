650761db9f6d8e7188d11395ea6fd75b37d2ebc5
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-12 23:59:41 +1000

    Added ProfileAbout and ProfileEditForm

---
+++ client-app/src/features/profiles/ProfileAbout.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { useStore } from '../../app/stores/store'
+import { Button, Grid, Header, Tab } from 'semantic-ui-react'
+import ProfileEditForm from './ProfileEditForm'
+import { observer } from 'mobx-react-lite'
+
+export default observer(function ProfileAbout() {
+  const { profileStore } = useStore()
+  const { isCurrentUser, profile } = profileStore
+  const [editMode, setEditMode] = useState(false)
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width='16'>
+          <Header
+            floated='left'
+            icon='user'
+            content={`About ${profile?.displayName}`}
+          />
+          {isCurrentUser && (
+            <Button
+              floated='right'
+              basic
+              content={editMode ? 'Cancel' : 'Edit Profile'}
+              onClick={() => setEditMode(!editMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width='16'>
+          {editMode ? (
+            <ProfileEditForm setEditMode={setEditMode} />
+          ) : (
+            // the style "whiteSpace: 'pre-wrap'" will preserve line breaks that are entered into the text area here.
+            <span style={{ whiteSpace: 'pre-wrap' }}>{profile?.bio}</span>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  )
+})
--- client-app/src/features/profiles/ProfileCard.tsx
+++ client-app/src/features/profiles/ProfileCard.tsx
@@ -9,12 +9,18 @@ interface Props {
 }
 
 const ProfileCard = ({ profile }: Props) => {
+  // truncate: hide the overflow text to keep card size small
+  function truncate(str: string | undefined) {
+    if (str) {
+      return str.length > 40 ? str.substring(0, 37) + '...' : str
+    }
+  }
   return (
     <Card as={Link} to={`/profiles/${profile.username}`}>
       <Image src={profile.image || '/assets/user.png'} />
       <Card.Content>
         <Card.Header>{profile.displayName}</Card.Header>
-        <Card.Description>Bio goes here</Card.Description>
+        <Card.Description>{truncate(profile.bio)}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Icon name='user' />
