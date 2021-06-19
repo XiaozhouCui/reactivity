4d05d0786faa402996d0755839275435f01a8f91
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-19 15:46:23 +1000

    Update controller to list followers/followees

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -7,11 +7,17 @@ namespace API.Controllers
     public class FollowController : BaseApiController
     {
         // send once will follow, send twice will un-follow (remove record from UserFollowings table)
-        [HttpPost("{username}")] // POST .../api/follow/tom
+        [HttpPost("{username}")] // POST .../api/follow/bob
         public async Task<IActionResult> Follow(string username)
         {
             // send a new instance of FollowToggle class from Application layer
-            return HandleResult(await Mediator.Send(new FollowToggle.Command{TargetUsername = username}));
+            return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+        }
+
+        [HttpGet("{username}")] // GET .../api/follow/bob?predicate=followers
+        public async Task<IActionResult> GetFollowings(string username, string predicate) // predicate from query string
+        {
+            return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
         }
     }
 }
--- Application/Followers/List.cs
+++ Application/Followers/List.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
-using Application.Profiles;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
