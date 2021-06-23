9ff0326117ed59327dc5f61eaa8de43d2b7d6e93
Joe Cui <xiaozhou.cui@gmail.com>
2021-06-23 21:49:23 +1000

    Adding vertical paging

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,26 +1,45 @@
 import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
-import { Grid } from 'semantic-ui-react'
+import { useEffect, useState } from 'react'
+import { Grid, Button } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
+import { PagingParams } from '../../../app/models/pagination'
 import { useStore } from '../../../app/stores/store'
 import ActitityFilters from './ActitityFilters'
 import ActivityList from './ActivityList'
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore()
-  const { loadActivities, activityRegistry } = activityStore
+  const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore
+  // local state to show loading of the next batch of activity
+  const [loadingNext, setLoadingNext] = useState(false)
+
+  // load next batch of activities
+  function handleGetNext() {
+    setLoadingNext(true)
+    setPagingParams(new PagingParams(pagination!.currentPage + 1)) // cause axios computed property to be updated
+    loadActivities().then(() => setLoadingNext(false))
+  }
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [activityRegistry.size, loadActivities])
 
-  if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />
+  // don't load the full page if it is only loading next batch of activities
+  if (activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />
 
 
   return (
     <Grid>
       <Grid.Column width='10'>
         <ActivityList />
+        <Button
+          floated='right'
+          content='More...'
+          positive
+          onClick={handleGetNext}
+          loading={loadingNext}
+          disabled={pagination?.totalPages === pagination?.currentPage}
+        />
       </Grid.Column>
       <Grid.Column width='6'>
         <ActitityFilters />
